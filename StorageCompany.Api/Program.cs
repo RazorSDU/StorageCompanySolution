5318a489990cdd66cea7d13a42fa619d22d0b47e
Casper Jensen <casjen01@easv365.dk>
2026-05-13 13:42:43 +0200

    Implementet login system

--- StorageCompany.Api/Program.cs
+++ StorageCompany.Api/Program.cs
@@ -77,6 +77,7 @@ public class Program
         builder.Services.AddScoped<IInvoiceService, InvoiceService>();
         builder.Services.AddScoped<IAccessCodeService, AccessCodeService>();
         builder.Services.AddScoped<ISupportRequestService, SupportRequestService>();
+        builder.Services.AddScoped<ISecurityService, SecurityService>();
 
         var app = builder.Build();
 
--- StorageCompany.Core/Entities/JwtClaims.cs
+++ StorageCompany.Core/Entities/JwtClaims.cs
@@ -4,6 +4,6 @@ public class JwtClaims
 {
     public required string Role { get; set; }
     public required string Email { get; set; }
-
+    public required string Exp { get; set; }
     public required string Id { get; set; }
 }
